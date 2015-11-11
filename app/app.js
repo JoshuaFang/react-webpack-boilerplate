fe4a8329b884e0156b01195fd4d315b931a55764
fangqian <fangqian@meituan.com>
2015-11-12 00:37:15 +0800

    delete files

--- app/app.js
+++
@@ -1,6 +0,0 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-import ProductTable from "./ProductTable.js";
-
-ReactDOM.render(<ProductTable/>, document.getElementById('content'));
